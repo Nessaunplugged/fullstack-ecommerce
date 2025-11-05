2b5d830ac42988be0db39efaa58b98e92f2dd89d
Agare Vanessa <Agarevanessa7@gmail.com>
2025-11-05 13:39:52 +0100

    Fix CORS for Netlify domain

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,7 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use((req, res, next) => {
-  const allowedOrigins = ['https://nuveestore.netlify.app', 'http://localhost:5173', 'http://localhost:5174'];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header('Access-Control-Allow-Origin', 'https://nuveestore.netlify.app');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
