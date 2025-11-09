0a9db4b23ef37bc3d15194536f71d324cf341da8
Agare Vanessa <Agarevanessa7@gmail.com>
2025-11-09 19:16:04 +0100

    Change Stripe currency from USD to NGN

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -40,7 +40,7 @@ export const createCheckoutSession = async (req, res) => {
 
       return {
         price_data: {
-          currency: "usd",
+          currency: "ngn",
           product_data: {
             name: product.name,
           },
